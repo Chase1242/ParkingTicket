e6a85007e32055dc490c9eec8519c274bef45b77
Chase1242 <chaseconaway82@gmail.com>
2024-05-10 20:46:59 -0700

    small changes, getting ready to generate schedules

---
+++ ParkingTickets.Shared/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkingTickets.Shared;
+
+public class Constants
+{
+    /// <summary>
+    /// The number of bins in a day. Represents all 24 hours, split into 5 minute chunks.
+    /// </summary>
+    public int Bins { get => (24 * 60) / 5; }
+}
